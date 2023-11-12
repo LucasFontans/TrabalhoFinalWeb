03bfeecbee2b970f391bdce2730278f1a02bfc4b
LucasFontans <lucasgbfontans@gmail.com>
2023-11-11 21:34:48 -0300

    funcionalidade de cadastro de chave

--- php/cadastrarChave.php
+++ php/cadastrarChave.php
@@ -1,8 +1,28 @@
 <?php
 session_start();
-    $_SESSION['id']
-    
-    echo "$id";
+$id = $_SESSION['id'];
+$servername = "localhost";
+$database = "piquesdb";
+$username = "root";
+$password = "";
 
+$chave = $_POST["chave"];
+$escolha = $_POST["escolha"];
 
+$conn = mysqli_connect($servername, $username, $password, $database);
+
+// Use consultas preparadas para evitar injeção de SQL
+$query2 = "UPDATE usuarios SET $escolha = '$chave' WHERE id = $id";
+
+// Executa a consulta diretamente
+if (mysqli_query($conn, $query2)) {
+    echo "<script>alert('Cadastro realizado com sucesso!')</script>;";
+    sleep(3);
+    header("Location: ../php/dashboard.php");
+} else {
+    echo "Erro na atualização: " . mysqli_error($conn);
+}
+
+// Fecha a conexão
+mysqli_close($conn);
 ?>
--- php/login.php
+++ php/login.php
@@ -10,18 +10,20 @@ function logar(){
     $email = $_POST["email"];
     $senha = $_POST["password"];
     $nameUser;
+    $id;
 
     $conn = mysqli_connect($servername, $username, $password, $database);
 
     $email = mysqli_real_escape_string($conn, $email);
     $senha = mysqli_real_escape_string($conn, $senha);
 
-    $query2 = "SELECT nomeUsuario FROM usuarios WHERE email = '$email'";
+    $query2 = "SELECT id, nomeUsuario FROM usuarios WHERE email = '$email'";
     $result2 = $conn->query($query2);
 
     if ($result2->num_rows == 1) {
         $row = $result2->fetch_assoc();
         $nameUser = $row['nomeUsuario'];
+        $id = $row['id'];
     } else {
         // Se não encontrar o usuário, você pode definir um valor padrão ou tomar outra ação, como redirecionar para a página de login
         $nameUser = "Usuário não encontrado";
@@ -31,6 +33,7 @@ function logar(){
     $result = $conn->query($query);
 
     if ($result->num_rows == 1) {
+        $_SESSION['id'] = $id;
         $_SESSION['userName'] = $nameUser;
         sleep(3);
         header("Location: ../php/dashboard.php");
